999838f109977eba0d344da84e519058c2483a6e
Creedyfish <ielbanbuenaofficial@gmail.com>
2024-03-14 03:00:35 +0800

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,37 @@
+import plaiceholder from "@plaiceholder/tailwindcss";
 import type { Config } from "tailwindcss";
+import {nextui} from "@nextui-org/react";
 
 const config: Config = {
   content: [
+   
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'primary-orange': 'hsl(26, 100%, 55%)',
+        'primary-pale-orange': 'hsl(25, 100%, 94%)',
+        'neutral-very-dark-blue': 'hsl(220, 13%, 13%)',
+        'neutral-dark-grayish-blue': 'hsl(219, 9%, 45%)',
+        'neutral-grayish-blue': 'hsl(220, 14%, 75%)',
+        'neutral-light-grayish-blue': 'hsl(223, 64%, 98%)',
+        'neutral-white': 'hsl(0, 0%, 100%)',
+        'neutral-black': 'hsl(0, 0%, 0%)',
+      },
+      opacity: {
+        '75': '0.75',
+      },
+      fontFamily: {
+        'kumbh': 'var(--kumbh_sans)',
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 };
 export default config;
