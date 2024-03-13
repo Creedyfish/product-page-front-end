e90b51a04b20d196fc27b1b1f40759ab58cf2e58
Creedyfish <ielbanbuenaofficial@gmail.com>
2024-03-14 03:00:50 +0800

    feat: Update layout and add ThemeSwitcher component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Kumbh_Sans } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
+import Navbar from "./components/Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
+const kumbh = Kumbh_Sans({ subsets: ["latin"], variable: "--kumbh_sans" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${kumbh.className}`}>
+        <Providers>
+          <Navbar />
+          {children}
+        </Providers>
+      </body>
     </html>
   );
 }
