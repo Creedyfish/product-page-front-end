47025e6d5329bd6d8ceb69e5bb6af271596313d4
Creedyfish <ielbanbuenaofficial@gmail.com>
2024-03-15 23:55:34 +0800

    feat: Refactor ThemeSwitcher component

--- app/components/ThemeSwitcher.tsx
+++ app/components/ThemeSwitcher.tsx
@@ -18,60 +18,23 @@ export function ThemeSwitcher() {
   if (!mounted) return null;
 
   return (
-    <div>
-      <Switch
-        isSelected={resolvedTheme === "light"}
-        size="md"
-        color="secondary"
-        onChange={() => {
-          {
-            resolvedTheme === "light" ? setTheme("dark") : setTheme("light");
-          }
-        }}
-        thumbIcon={({ isSelected, className }) =>
-          isSelected ? (
-            <SunIcon className={className} />
-          ) : (
-            <MoonIcon className={className} />
-          )
+    <Switch
+      className=""
+      isSelected={resolvedTheme === "light"}
+      size="md"
+      color="primary"
+      onChange={() => {
+        {
+          resolvedTheme === "light" ? setTheme("dark") : setTheme("light");
         }
-      ></Switch>
-      {/* {theme === "light" ? (
-        <button onClick={() => setTheme("dark")} className="flex gap-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="h-6 w-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
-            />
-          </svg>
-          <div className="text-red-500">Dark Mode</div>
-        </button>
-      ) : (
-        <button onClick={() => setTheme("light")}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="h-6 w-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
-            />
-          </svg>
-        </button>
-      )} */}
-    </div>
+      }}
+      thumbIcon={({ isSelected, className }) =>
+        isSelected ? (
+          <SunIcon className={className} />
+        ) : (
+          <MoonIcon className={className} />
+        )
+      }
+    ></Switch>
   );
 }
