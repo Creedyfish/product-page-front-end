8c5a3dd9a953245fa95e01a44963f23b430454e8
Creedyfish <ielbanbuenaofficial@gmail.com>
2024-03-15 23:54:35 +0800

    feat: Remove unused font import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter, Kumbh_Sans } from "next/font/google";
+import { Kumbh_Sans } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import Navbar from "./components/Navbar";
